3ba0478155bafd320edfe369dbf709e0c81865b3
sbindreiter <33084146+sbindreiter@users.noreply.github.com>
2018-01-06 12:44:24 +0100

    zwischenstand 06.01.

--- main.js
+++ main.js
@@ -1,34 +1,34 @@
 import 'ol/ol.css';
+import ol from 'ol/control/control';
 //import 'javascript-autocomplete/auto-complete.css'; //additional includes für UE4
-// import View from 'ol/view';
+//import View from 'ol/view';
 //import Map from 'ol/map';
+
 // import TileLayer from 'ol/layer/tile';
 // import Stamen from 'ol/source/stamen';
 import VectorLayer from 'ol/layer/vector';
 import Vector from 'ol/source/vector';
-//import VectorSource from 'ol/source/vector';
+import VectorSource from 'ol/source/vector';
 import GeoJSON from 'ol/format/geojson';
 import Style from 'ol/style/style';
-//import IconStyle from 'ol/style/icon';
-import Circle from 'ol/style/circle'; //ueberfluessiges include aus UE3
+import IconStyle from 'ol/style/icon';
+//import Circle from 'ol/style/circle';
 //import Text from 'ol/style/text';
-import Fill from 'ol/style/fill';
-import Stroke from 'ol/style/stroke';
+//import Fill from 'ol/style/fill';
+//import Stroke from 'ol/style/stroke';
 import proj from 'ol/proj';
 import {
   apply
 } from 'ol-mapbox-style';
-//import AutoComplete from 'javascript-autocomplete'; //additional includes für UE4
+import AutoComplete from 'javascript-autocomplete'; //additional includes für UE4
 import Overlay from 'ol/overlay';
 //import coordinate from 'ol/coordinate';
-//import {ValidCaptcha} from './captcha.js';
-//import {Captcha} from './captcha.js';
 
 
 function validateForm() {
   var name = document.forms['geowebInsert']['name'].value;
   var email = document.forms['geowebInsert']['email'].value;
-  var place = document.forms['geowebInsert']['place'].value;  
+  var place = document.forms['geowebInsert']['place'].value;
   if (name == '') {
     alert('Name ist Pflichtfeld!');
     return false;
@@ -41,20 +41,52 @@ function validateForm() {
   }
 }
 
+//BASE MAP
 const map = apply(
   'map',
   'style.json'
 );
 
+//ADRESS LOOKUP
+var searchResult = new VectorLayer({
+  zIndex: 1
+});
+map.addLayer(searchResult);
+
+new AutoComplete({
+  selector: 'input[name="q"]',
+  source: function(term, response) {
+    var source = new VectorSource({
+      format: new GeoJSON(),
+      url: 'https://photon.komoot.de/api/?q=' + term
+    });
+    source.on('change', function() {
+      var texts = source.getFeatures().map(function(feature) {
+        var properties = feature.getProperties();
+        return (properties.city || properties.name || '') + ', ' +
+          (properties.street || '') + ' ' +
+          (properties.housenumber || '');
+      });
+      response(texts);
+      map.getView().fit(source.getExtent(), {
+        maxZoom: 19,
+        duration: 250
+      });
+    });
+    searchResult.setSource(source);
+  }
+});
+
+//OVERLAYS-POPUPS
 var overlay = new Overlay({
   id: 'info',
-  element: document.getElementById('popup-container'),
+  element: document.getElementById('popupinfo'),
   positioning: 'bottom-center',
   offset: [0, -10]
 });
 var overlay2 = new Overlay({
   id: 'insertForm',
-  element: document.getElementById('popup-container2'),
+  element: document.getElementById('popupform'),
   positioning: 'bottom-center',
   offset: [0, -10]
 });
@@ -74,16 +106,38 @@ const bezirkeLayer = new VectorLayer({
 });
 map.addLayer(bezirkeLayer);
 
-const ruheLayer = new VectorLayer({
+const ruheLayer1 = new VectorLayer({
+  id: 'r1',
   source: new Vector({
-    url: 'https://student.ifip.tuwien.ac.at/geoweb/2017/g07/map/postgis_li_geojson.php?type=R',
+    url: 'https://student.ifip.tuwien.ac.at/geoweb/2017/g07/map/postgis_li_geojson.php?type=R&cat=1',
     format: new GeoJSON()
   }),
   zIndex: 3
 });
-map.addLayer(ruheLayer);
+map.addLayer(ruheLayer1);
+
+const ruheLayer2 = new VectorLayer({
+  id: 'r2',
+  source: new Vector({
+    url: 'https://student.ifip.tuwien.ac.at/geoweb/2017/g07/map/postgis_li_geojson.php?type=R&cat=2',
+    format: new GeoJSON()
+  }),
+  zIndex: 3
+});
+map.addLayer(ruheLayer2);
+
+const ruheLayer3 = new VectorLayer({
+  id: 'r3',
+  source: new Vector({
+    url: 'https://student.ifip.tuwien.ac.at/geoweb/2017/g07/map/postgis_li_geojson.php?type=R&cat=3',
+    format: new GeoJSON()
+  }),
+  zIndex: 3
+});
+map.addLayer(ruheLayer3);
 
 const laermLayer = new VectorLayer({
+  id: 'l1',
   source: new Vector({
     url: 'https://student.ifip.tuwien.ac.at/geoweb/2017/g07/map/postgis_li_geojson.php?type=L',
     format: new GeoJSON()
@@ -92,8 +146,15 @@ const laermLayer = new VectorLayer({
 });
 map.addLayer(laermLayer);
 
-ruheLayer.setStyle(function(feature) {
+//map.addControl(new ol.Control.LayerSwitcher());
+
+ruheLayer1.setStyle(function(feature) {
   return new Style({
+    image: new IconStyle({
+      src: './data/marker_oase1.png',
+      anchor: [0.5, 1]
+    })
+    /*
     image: new Circle({
       radius: 7,
       fill: new Fill({
@@ -104,32 +165,41 @@ ruheLayer.setStyle(function(feature) {
         width: 1
       })
     })
+    */
+  });
+});
+
+ruheLayer2.setStyle(function(feature) {
+  return new Style({
+    image: new IconStyle({
+      src: './data/marker_oase2.png',
+      anchor: [0.5, 1]
+    })
+  });
+});
+
+ruheLayer3.setStyle(function(feature) {
+  return new Style({
+    image: new IconStyle({
+      src: './data/marker_oase3.png',
+      anchor: [0.5, 1]
+    })
   });
 });
 
 laermLayer.setStyle(function(feature) {
   return new Style({
-    image: new Circle({
-      radius: 7,
-      fill: new Fill({
-        color: 'rgba(232, 12, 12, 1)'
-      }),
-      stroke: new Stroke({
-        color: 'rgba(127, 127, 127, 1)',
-        width: 1
-      })
+    image: new IconStyle({
+      src: './data/marker_noise1.png',
+      anchor: [0.5, 1]
     })
   });
 });
 
-// var form = document.getElementById('geowebInsert');
-// document.getElementById('chkAndSend').addEventListener('singleclick', function() {
-//   if (ValidCaptcha()) {
-//     form.submit();
-//   }
-//
-// });
-
+ruheLayer1.set('name', 'r1', true);
+ruheLayer2.set('name', 'r2', true);
+ruheLayer3.set('name', 'r3', true);
+laermLayer.set('name', 'l1', true);
 
 map.on('singleclick', function(e) {
   var markup = '';
@@ -138,79 +208,69 @@ map.on('singleclick', function(e) {
     markup += `${markup && '<hr>'}<table>`;
     for (var property in properties) {
       if (property != 'geometry') {
-        markup += `<tr><th>${property}</th><td>${properties[property]}</td></tr>`;
+        markup += `<tr><th class="ruhe">${property}</th><td>${properties[property]}</td></tr>`;
       }
     }
     markup += '</table>';
   }, {
-    layerFilter: (l) => l === ruheLayer || l === laermLayer
+    layerFilter: (l) => l === ruheLayer1 || l === ruheLayer2 || l === ruheLayer3
   });
+  if (!markup) {
+    map.forEachFeatureAtPixel(e.pixel, function(feature) {
+      var properties = feature.getProperties();
+      markup += `${markup && '<hr>'}<table>`;
+      for (var property in properties) {
+        if (property != 'geometry') {
+          markup += `<tr><th class="laerm">${property}</th><td>${properties[property]}</td></tr>`;
+        }
+      }
+      markup += '</table>';
+    }, {
+      layerFilter: (l) => l === laermLayer
+    });
+  }
   if (markup) {
-    document.getElementById('popup-content').innerHTML = markup;
+    document.getElementById('popupinfo-content').innerHTML = markup;
     overlay.setPosition(e.coordinate);
     overlay2.setPosition();
   } else {
     overlay.setPosition();
     overlay2.setPosition();
     var pos = proj.toLonLat(e.coordinate);
-    //ALTER AUFRUF EXTERNE SEITE
-    //window.location.href =
-    //  'https://student.ifip.tuwien.ac.at/geoweb/2017/g07/php/insert_point.php?pos=' +
-    //  pos.join(' ');
-    //TEST AUFRUF
-    //markup += `${markup && '<hr>'}<p>TEST Pos:${pos.join(' ')}</p>`;
     // NEUER AUFRUF POPUP MARKUP
     markup += `${markup && '<hr>'}`;
-    markup += '<h2>Ruheoase oder Lärmort</h2>';
-    markup += '<p class="footnote"><font color="red">* Pflichtfeld</font></p>';
-    markup += `<form class="content" id="geowebInsert" onreset="return removeForm();" onsubmit="return validateForm();" method="POST" action="https://student.ifip.tuwien.ac.at/geoweb/2017/g07/php/insert_point.php?pos=${pos.join(' ')}">`;
-    markup += '         <fieldset>';
-    markup += '  <input class="content" type="radio" id="laerm1" name="laerm" value="R" checked="checked">';
-    markup += '  <label class="content" for="laerm1">Ruheoase</label>';
-    markup += '  <input class="content" type="radio" id="laerm2" name="laerm" value="L">';
-    markup += '  <label class="content" for="laerm2">Lärmzone</label>';
-    markup += '</fieldset>';//'<p style="color:red">*</p>';
-    markup += '<fieldset>';
-    markup += '  <input class="content" type="radio" id="privacat1" name="privcat" value="1" checked="checked">';
-    markup += '  <label class="content" for="privacat1">öffentlich</label>';
-    markup += '    <input class="content" type="radio" id="privacat2" name="privcat" value="2">';
-    markup += '    <label class="content" for="privacat2">halböffentlich</label>';
-    markup += '  <input class="content" type="radio" id="privacat3" name="privcat" value="3">';
-    markup += '  <label class="content" for="privacat3">privat</label>';
-    markup += '  </fieldset>';//'<p style="color:red">*</p>';
-    markup += '<table class="insertForm">';
-    markup +=  '<tr><td>Name</td><td><input type="text" name="name" /><font color="red">*</font>(wird nicht angezeigt)</td></tr>';
-    markup +=  '<tr><td>Email</td><td><input type="text" name="email" /><font color="red">*</font></td></tr>';
-    markup +=  '<tr><td>Ort</td><td><input type="text" name="place" /><font color="red">*</font></td></tr>';
-    markup +=  '<tr><td>Beschreibung</td><td><input type="text" name="desc" /></td></tr>';
-    markup += ' <tr><td><input id ="cancel" type="reset" value="cancel"></td>';
-    markup += ' <td><input type="submit" value="send"></td></tr></table></form>';
-
-    //  markup += '  <div class="inputForm capt"><h3 type="text" id="mainCaptcha"></h3><br>';
-    //  markup += '  <input class="inputForm" type="button" id="refresh" value="refresh" onclick="Captcha();"/><br>';
-    //  markup += '  <input class="inputForm" type="text" name="txtInput" id="txtInput"/>';
-    //  markup += '  <br><input class="inputForm" id="chkAndSend" type="button" value="send" onclick="CheckAndSend;"/>';  /*onclick="alert(ValidCaptcha());"*/
-    //  markup += '  <br><input class="inputForm" id="cancel" type="reset" value="cancel" onclick="overlay2.setPosition();"/></div>';
-    /*https://stackoverflow.com/questions/3196335/how-to-create-simple-javascript-jquery-client-side-captcha
-
-    <div class="capt">
-    <h2 type="text" id="mainCaptcha"></h2>
-    <p><input type="button" id="refresh" onclick="Captcha();"/></p>
-    <input type="text" id="txtInput"/>
-    <input id="Button1" type="button" value="Check" onclick="alert(ValidCaptcha());"/>
-    </div>
-    */
-    //markup += `  <input type="hidden" name="pos" value="${pos.join(' ')}">`; //WENN WIRS ALS POST VARIABLE SCHICKEN WOLLEN - ABER NET NOTWENDIG
-    //markup += '  </form></td></tr></table>';
+    markup += '<h2>Ruheoase <font color="black">oder</font> <font color="#E62E00">Lärmort</font></h2>';
+    markup += `<form class="insertForm" id="geowebInsert" onreset="return removeForm();" onsubmit="return validateForm();" method="POST" action="https://student.ifip.tuwien.ac.at/geoweb/2017/g07/php/insert_point.php?pos=${pos.join(' ')}">`;
+    markup += '<fieldset class="insertForm">';
+    markup += '  <input type="radio" id="laerm1" name="laerm" value="R" checked="checked">';
+    markup += '  <label for="laerm1">Ruheoase</label>';
+    markup += '  <input type="radio" id="laerm2" name="laerm" value="L">';
+    markup += '  <label for="laerm2">Lärmzone</label>';
+    markup += '</fieldset>';
+    markup += '<fieldset class="insertForm">';
+    markup += '  <input type="radio" id="privacat1" name="privcat" value="1" checked="checked">';
+    markup += '  <label for="privacat1">öffentlich</label>';
+    markup += '    <input type="radio" id="privacat2" name="privcat" value="2">';
+    markup += '    <label for="privacat2">halböffentlich</label>';
+    markup += '  <input type="radio" id="privacat3" name="privcat" value="3">';
+    markup += '  <label for="privacat3">privat</label>';
+    markup += '  </fieldset>';
+    markup += '<table>';
+    markup +=  '<tr><td class="laerm">Name<font color="red"> * Pflichtfeld</font></td><td><input class="laerm" type="text" name="name" /></td></tr>';
+    markup +=  '<tr><td class="laerm">Email<font color="red"> *</font></td><td><input class="laerm" type="text" name="email" />(wird nicht angezeigt)</td></tr>';
+    markup +=  '<tr><td class="laerm">Ort<font color="red"> *</font></td><td><input class="laerm" type="text" name="place" /></td></tr>';
+    markup +=  '<tr><td class="laerm">Beschreibung</td><td><textarea rows="3" id="inputFormdesc" name="desc" wrap="hard"/>Beschreibung hinzufügen</textarea></td></tr>';
+    markup += ' <tr><td></td>';
+    markup += ' <td><input id ="cancel" type="reset" value="cancel"><input type="submit" value="send"></td></tr></table></form>';
     //MARKUP ANZEIGEN
-    document.getElementById('popup-content2').innerHTML = markup;
+    document.getElementById('popupform-content').innerHTML = markup;
     overlay2.setPosition(e.coordinate);
 
     (function() {
       var qs = function(s) {
-          return document.querySelector(s);
-        },
-      form = qs('form');
+            return document.querySelector(s);
+          },
+          form = qs('form');
       form.onsubmit = function(e) {
         return validateForm();
       };
@@ -218,93 +278,22 @@ map.on('singleclick', function(e) {
         return removeForm();
       };
     }());
-
-    /*
-    document.getElementById('refresh').addEventListener('singleclick', function() {
-      Captcha();
-    });
-
-    document.getElementById('chkAndSend').addEventListener('singleclick', function() {
-      if (ValidCaptcha()) {
-        var form = document.getElementById('geowebInsert');
-        form.submit();
-      }
-    });
-
-    document.getElementById('cancel').addEventListener('singleclick', function() {
-      var form = document.getElementById('geowebInsert');
-      form.reset();
-      overlay.setPosition();
-      overlay2.setPosition();
-    });
-    */
   }
 });
 
-//ALTER SOURCECODE - vielleicht brauchma no ----------------------------------------------------------------------------------------
-//ue4
-//var onload, source;
-// new AutoComplete({
-//   selector: 'input[name="q"]',
-//   source: function(term, response) {
-//     if (onload) {
-//       source.un('change', onload);
-//     }
-//     searchResult.setSource(null);
-//     source = new VectorSource({
-//       format: new GeoJSON(),
-//       url: 'https://photon.komoot.de/api/?q=' + term
-//     });
-//     onload = function(e) {
-//       var texts = source.getFeatures().map(function(feature) {
-//         return getAddress(feature);
-//       });
-//       response(texts);
-//       fit();
-//     };
-//     source.once('change', onload);
-//     searchResult.setSource(source);
-//   },
-//   onSelect: function(e, term, item) {
-//     selected = item.getAttribute('data-val');
-//     source.getFeatures().forEach(function(feature) {
-//       if (getAddress(feature) !== selected) {
-//         source.removeFeature(feature);
-//       }
-//     });
-//     fit();
-//   }
-// });
 
-// UE4
-// function fit() {
-//   map.getView().fit(source.getExtent(), {
-//     maxZoom: 19,
-//     duration: 250
-//   });
-// }
+function bindInputs(layer) {
+  if (layer.get('name') === 'r1' || layer.get('name') === 'r2' || layer.get('name') === 'r3' || layer.get('name') === 'l1') {
+    var visibilityInput = $(layer.get('name'));
+    visibilityInput.on('change', function() {
+      layer.setVisible(this.checked);
+    });
+    visibilityInput.prop('checked', layer.getVisible());
+  }
+}
 
-// var selected;
-//
-// function getAddress(feature) {
-//   var properties = feature.getProperties();
-//   return (
-//     (properties.city || properties.name || '') +
-//     ' ' +
-//     (properties.street || '') +
-//     ' ' +
-//     (properties.housenumber || '')
-//   );
-// }
-
-/*suchfunktion UE4*/
-// var searchResult = new VectorLayer({
-//   zIndex: 3 //
-// });
-//
-// map.addLayer(searchResult);
-// searchResult.setStyle(new Style({
-//   image: new IconStyle({
-//     src: './data/marker1.png'
-//   })
-// }));
+//function inputChanged() {
+  map.getLayers().forEach(function(layer, i) {
+    bindInputs(layer);
+  });
+//}
